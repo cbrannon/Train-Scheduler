29527df7e04327e75595f947c1b455104ada9802
Cody Brannon <codydbrannon@gmail.com>
2017-03-15 21:25:05 -0400

    Added php file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,34 +9,41 @@ $(document).ready(function () {
   };
   firebase.initializeApp(config);
 
-  var database = firebase.database();
-  var trains = database.ref("trains");
+  var database = firebase.database(),
+      trains = database.ref("trains");
+  
+   var trainData = '',
+        name = '',
+        destination = '',
+        firstTrain = '',
+        frequency = '';
 
   trains.on("child_added", function (childSnapshot) {
-      var trainData = childSnapshot.val();
-      var name = trainData.name;
-      var destination = trainData.destination;
-      var firstTrain = trainData.first_train;
-      var frequency = trainData.frequency;
-      buildSchedule(name, destination, firstTrain, frequency);
+    trainData = childSnapshot.val(),
+    name = trainData.name,
+    destination = trainData.destination,
+    firstTrain = trainData.first_train,
+    frequency = trainData.frequency;
+    
+    buildSchedule(name, destination, firstTrain, frequency);
+    
   }, errData);
   function errData(data) {
     console.log(data);
   }
 
   function buildSchedule(name, destination, firstTrain, frequency) {
-    var newRow = $("<tr>");
-    var mdlCellClass = "mdl-data-table__cell--non-numeric";
+    var newRow = $("<tr>"),
+        mdlCellClass = "mdl-data-table__cell--non-numeric",
+        nextTrain = getNextTrain(firstTrain, frequency);
 
-    // var nextTrain = getNextTrain(firstTrain, frequency);
-    var nextTrain = getNextTrain(firstTrain, frequency);
     console.log(getNextTrain(firstTrain, frequency));
 
-    var nameCell = $("<td>").addClass(mdlCellClass).html(name);
-    var destCell = $("<td>").addClass(mdlCellClass).html(destination);
-    var freqCell = $("<td>").addClass(mdlCellClass).html(frequency);
-    var nextCell = $("<td>").addClass(mdlCellClass).html(nextTrain);
-    var awayCell = $("<td>").addClass(mdlCellClass).html(moment(nextTrain, 'h:mm A').fromNow());
+    var nameCell = $("<td>").addClass(mdlCellClass).html(name),
+        destCell = $("<td>").addClass(mdlCellClass).html(destination),
+        freqCell = $("<td>").addClass(mdlCellClass).html(frequency),
+        nextCell = $("<td>").addClass(mdlCellClass).html(nextTrain),
+        awayCell = $("<td>").addClass(mdlCellClass).html(moment(nextTrain, 'h:mm A').fromNow());
 
     newRow
       .append(nameCell)
@@ -48,26 +55,16 @@ $(document).ready(function () {
     $("#schedule").append(newRow);
   }
 
-// The first train of the day comes in at 3:00 AM.
-// The train runs every 17 minutes
-// The current time is 7:12 PM.
-// There have been no delays and will be no delays.
-
-// currentTime - startTime = Total minutes between
-// totalMinutes % interval = minutes into next train
-// interval - minutes into gives us time of next train  
-
   function getNextTrain(firstTrain, interval) {
-    var now = moment();
-    var hours = firstTrain.substr(0, 2);
-    var minutes = firstTrain.substr(3, 4);
-    var train = moment().startOf('day').hour(parseInt(hours)).minute(parseInt(minutes));
-    var duration = moment.duration(now.diff(train)).asMinutes();
-    var minutesUntil = duration % interval;
-    var nextTrain = interval - minutesUntil;
-    var newTrain = now.add(nextTrain, "minutes").format("HH:MM");
+    var now = moment(),
+        hours = firstTrain.substr(0, 2),
+        minutes = firstTrain.substr(3, 4),
+        train = moment().startOf('day').hour(parseInt(hours)).minute(parseInt(minutes)),
+        duration = moment.duration(now.diff(train)).asMinutes(),
+        minutesUntil = duration % interval,
+        nextTrain = interval - minutesUntil,
+        newTrain = now.add(nextTrain, "minutes").format("HH:MM");
     
-    // return train.format("HH:MM");
     return moment(now).format("h:mm a");
   }
 
@@ -78,11 +75,12 @@ $(document).ready(function () {
     $("#frequency-input").val("");
   }
 
-  $("#submit").on("click", function () {
-    var trainName = $("#train-name-input").val().trim();
-    var destination = $("#destination-input").val().trim();
-    var first_train = $("#first-train-input").val().trim();
-    var frequency = $("#frequency-input").val().trim();
+  $("#submit").on("click", function (event) {
+    event.preventDefault();
+    var trainName = $("#train-name-input").val().trim(),
+        destination = $("#destination-input").val().trim(),
+        first_train = $("#first-train-input").val().trim(),
+        frequency = $("#frequency-input").val().trim();
 
     if (trainName != "" || destination != "" || first_train != "" || frequency != "") {
 
